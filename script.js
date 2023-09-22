88839caa6952fe087902b65131ab1f385b7480c6
rohnshrma <rohn11shrma@gmail.com>
2023-09-22 21:07:06 +0530

    upto variables

---
+++ script.js
@@ -0,0 +1,49 @@
+// output methods
+
+// 1. console.log() function => shows the output in the console tab of the browser
+
+// console.log("hello ruchi")
+// console.log(2+2)
+// console.log(2>0)
+
+
+// console.log(window)
+
+
+// window.alert("hello worldX")
+// alert("hello worldX")
+
+
+// console.log(document)
+
+
+// document.write("hello world")
+// document.write("<h2>hello world</h2>")
+
+
+
+// input 
+
+
+// prompt("Enter your name : ","Username") //message, default
+
+
+// ========================= variables
+
+// declaration
+// intialization / assigning a value
+
+
+// "var" keyword is used to create a variable in js
+
+// var age = 12; // variable declaration , identifier / variable name , assignment operator , value , EOS
+
+
+// var age; //declaration
+
+// age = 23; // intialization / assignment of a value
+
+// console.log(age)
+
+// age = 34; // re-assignment
+// console.log(age)
