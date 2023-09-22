7f98e1861efe86ccbfab42cb2682fd860324f9c9
rohnshrma <rohn11shrma@gmail.com>
2023-09-22 22:02:19 +0530

    data types(p&r) , string

---
+++ dataTypes.js
@@ -0,0 +1,109 @@
+// console.log(x)
+// how to check the data type of any value or variable
+
+// typeof keyword
+
+
+// console.log(typeof "hello world");
+// console.log(typeof 123);
+// console.log(typeof 1.23);
+// console.log(typeof true);
+// console.log(typeof false);
+
+
+// var myAge = "ruchi"
+// console.log(typeof myAge);
+
+
+// primitve data types
+
+// var a = 10;
+
+// assiged a copy of value of a to b
+// var b = a; // 10
+
+// console.log(a)
+// console.log(b)
+
+// a = 20;
+
+// console.log(a)
+// console.log(b)
+
+
+
+// var marks = [12,12,34,56,67,89,0];
+// var new_marks = marks; //reference
+
+// console.log(marks);
+// console.log(new_marks);
+
+
+// marks[2] = 56;
+
+// console.log(marks);
+// console.log(new_marks);
+
+
+
+
+// =================================================
+
+// var x ;
+
+// abscence of value / unintentional
+// console.log(x);
+
+
+
+// function greet(name){
+//     console.log("welcome mr ", name)
+// }
+
+// greet()
+
+
+// null // intentional abscence of value
+
+
+// var y = null
+
+
+// y= 12
+// console.log(y);
+
+
+
+
+// =========================
+//  var res = 12 < 10;
+//  console.log(res);
+
+
+
+
+
+// console.log("124" + 123) // string 124123
+// console.log("124" + "123") // string 124123
+// console.log(123 + 123) //number 246
+
+// console.log(+ "123" + 123) 
+// console.log(- "123" + 123) 
+
+
+// var x  = 12;
+
+// var x;
+// x = 12
+
+
+// const pi  = 3.14;
+// pi = 123;
+// console.log(pi);
+
+
+
+// assigning a value is compulsory
+// declaration and intialization
+// const pi;
+
---
+++ string.js
@@ -0,0 +1,27 @@
+//  a sequence of characters (alpha,num,symbols(including whitespace)) enclosed in single or double quotes
+
+// length ?
+
+var mess = "hello world this is ruchi"
+// console.log(mess.length)
+// console.log("hello world".length)
+
+// console.log(typeof mess);
+
+
+
+// first char/item is always on the "0" position/index
+// last char/item is alwaus on the len - 1  position/index 
+
+// concatenation
+
+
+var fName = "ruchi"
+var lName = "sharma"
+var age = 123;
+
+
+// console.log("hello world my name is " + fName + " " + lName + " and my age is " + age)
+console.log(`hello world my name is ${fName}  ${lName}  and my age is ${age - 10} old.
+`)
+
